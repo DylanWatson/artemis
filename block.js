71aeba64bcc605859f4afba6800087d861c4e062
Dylan Watson <watson93@gmail.com>
2018-05-07 18:53:48 -0400

    Removed customizing for now

--- block.js
+++ block.js
@@ -1,44 +1,10 @@
-var listOfUrls = [];
-determineUrlsThatShouldBeBlocked();
-function determineUrlsThatShouldBeBlocked()
-{
-  console.log("determining urls...");
-  chrome.storage.sync.get({
-    facebook: false,
-    youtube: false,
-    twitter: false,
-    instagram: false
-  }, function(items) {
-    console.log("checking facebook");
-    if(items.facebook === true)
-    {
-      listOfUrls.push("*://www.facebook.com/*");
-    }
-    if(items.youtube === true)
-    {
-      listOfUrls.push("*://www.youtube.com/*");
-    }
-    if(items.twitter === true)
-    {
-      listOfUrls.push("*://www.twitter.com/*");
-    }
-    if(items.instagram === true)
-    {
-      listOfUrls.push("*://www.instagram.com/*");
-    }
-  });
-}
-
-console.log(listOfUrls);
-
+let urlsToBeBlocked = ["*://www.facebook.com/*", "*://www.youtube.com/*", "*://www.youtube.com/*", "*://www.twitter.com/*", "*://www.instagram.com/*"];
 chrome.webRequest.onBeforeRequest.addListener(
     function() {
-      console.log("blocking!");
-      console.log(listOfUrls);
-        return {cancel: true};
+        return {redirectUrl: "http://www.google.com"};
     },
     {
-        urls: listOfUrls
+        urls:urlsToBeBlocked
     },
     ["blocking"]
 );
