fe297f148da0efddbdf277a48ceba05e2cbe10e3
Dylan Watson <watson93@gmail.com>
2018-05-11 21:53:14 -0400

    Redirects to local url:

--- block.js
+++ block.js
@@ -1,7 +1,7 @@
 let urlsToBeBlocked = ["*://www.facebook.com/*", "*://www.youtube.com/*", "*://www.youtube.com/*", "*://www.twitter.com/*", "*://www.instagram.com/*"];
 chrome.webRequest.onBeforeRequest.addListener(
     function() {
-        return {redirectUrl: "http://www.google.com"};
+        return {redirectUrl: "http://localhost:3000"};
     },
     {
         urls:urlsToBeBlocked
